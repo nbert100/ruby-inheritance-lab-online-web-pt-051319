082417c0ba2fe97978d4e51573e939cb1119f2ff
Nia Bert <nbert14@gmail.com>
2019-06-13 00:40:08 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,4 +3,10 @@ class User
     @first_name = first_name
     @last_name = last_name
   end
+  
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
 end
