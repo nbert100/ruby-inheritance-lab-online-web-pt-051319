b7ed98f635c2ab61a635a5502bc188edd9d1cedb
Nia Bert <nbert14@gmail.com>
2019-06-13 00:43:34 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,12 @@
 class User 
+  attr_accessor :first_name, :last_name
   def initialize
     @first_name = first_name
     @last_name = last_name
   end
   
-  def name=(full_name)
-    first_name, last_name = full_name.split
+  # def name=(full_name)
+  #   first_name, last_name = full_name.split
     
-  end
+  # end
 end
